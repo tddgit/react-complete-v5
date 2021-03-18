431bd1d020ff744221244d9bf0b1982574d8e68a
Stas <holidayonmail@gmail.com>
2021-03-18 15:27:21 +0300

    1

--- src/Pet.js
+++ src/Pet.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prop-types
 /**
  * @param root0
  * @param root0.name
@@ -8,13 +7,21 @@ import React from 'react';
  * @param root0.breed
  * @example
  */
+// eslint-disable-next-line react/prop-types
 export default function Pet({ name, animal, breed }) {
-    return React.createElement('div', {}, [
-        // eslint-disable-next-line react/prop-types
-        React.createElement('h1', {}, name),
-        // eslint-disable-next-line react/prop-types
-        React.createElement('h1', {}, animal),
-        // eslint-disable-next-line react/prop-types
-        React.createElement('h1', {}, breed),
-    ]);
+    // return React.createElement('div', {}, [
+    //     // eslint-disable-next-line react/prop-types
+    //     React.createElement('h1', {}, name),
+    //     // eslint-disable-next-line react/prop-types
+    //     React.createElement('h1', {}, animal),
+    //     // eslint-disable-next-line react/prop-types
+    //     React.createElement('h1', {}, breed),
+    // ]);
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{animal}</h2>
+            <h3>{breed}</h3>
+        </div>
+    );
 }
